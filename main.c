6e5ed3182a6253e30278cffc1c85a320692ce660
023javi <javier023javi@gmail.com>
2024-11-14 00:22:18 +0100

    feat: add rand_DNI method

--- main.c
+++ main.c
@@ -5,7 +5,8 @@
 #include <time.h>
 
 #define N 100
-void rand_dig (char *);
+
+void rand_dig(char *);
 void rand_DNI (unsigned *);
 void rand_DNIs (unsigned [N], char [N]);
 void print_DNIs (unsigned [N], char [N]);
@@ -13,12 +14,27 @@ void swap_unsigned(unsigned*, unsigned*);
 void swap_char(char*, char*);
 void bubbleSort(unsigned [N], char [N]);
 
+
 void rand_dig(char *dig) {
     *dig = rand() % 10 + '0';
 }
 
+void rand_DNI(unsigned *DNI) {
+    char dig;
+    *DNI = 0;
+    for (int i = 0; i < 8; i++) {
+        rand_dig(&dig);
+        *DNI = *DNI * 10 + (dig - '0');
+    }
+
+}
+
+
 int main(void)
 {
     srand(time(NULL));
+    unsigned DNI;
+    rand_DNI(&DNI);
+    printf("DNI: %u\n", DNI);
     return 0;
 }
