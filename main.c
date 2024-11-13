88f7d28ab91e9af5c29ea544f793b840de4fb18d
023javi <javier023javi@gmail.com>
2024-11-14 00:04:06 +0100

    Add

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <conio.h>
+#include <time.h>
+
+#define N 100
+void rand_dig (char *);
+void rand_DNI (unsigned *);
+void rand_DNIs (unsigned [N], char [N]);
+void print_DNIs (unsigned [N], char [N]);
+void swap_unsigned(unsigned*, unsigned*);
+void swap_char(char*, char*);
+void bubbleSort(unsigned [N], char [N]);
+
+int main(void)
+{
+    printf("Hello, World!\n");
+    return 0;
+}
